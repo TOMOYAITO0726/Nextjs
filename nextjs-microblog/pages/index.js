dec19bafb354677256675bf9f8d9e9340ac7d7f0
Tomoya Ito <tomoyaito199507265724@gmail.com>
2023-11-07 07:49:44 +0900

    トップページレイアウト

--- nextjs-microblog/pages/index.js
+++ nextjs-microblog/pages/index.js
@@ -3,13 +3,30 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import utilStyle from "../styles/utils.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={utilStyle.headingMd}>
         <p>私はフルスタックエンジニアです</p>
+      </section>
+
+      <section>
+        <h2>エンジニアのブログ</h2>
+        <div>
+          <article>
+            <Link legacyBehavior href="/">
+              <img src='/images/thumbnail01.jpg' alt=""/>
+            </Link>
+            <Link legacyBehavior href="/">
+            <a>SSGとSSRの使い分けの場面はいつなのか？</a>
+            </Link>
+            <br />
+            <small>February 23, 2020</small>
+          </article>
+        </div>
       </section>
     </Layout>
   );
